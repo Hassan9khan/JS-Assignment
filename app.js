25bb5d5fac2d51a1a55dbeee10eaaaaaca69b4d4
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-02-27 14:11:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// alert("Hello! how are you!!");
+
+// alert("Error! Please enter a valid password ");
+
+// alert('welcome to JS Land \n HAPPY CODING');
+
+// alert('Welcome to JS Land');
+
+// alert("HAPPY CODING");
+
+// var username = 'Hassan Ahmed '
+
+// var myName = 'Hassan Ahmed'
+// console.log(myName)
+
+// var message;
+// message = "hello world";    
+// alert(message);
+
+// var message;
+// message = "John Wick";    
+// alert(message);
+
+// var message;
+// message = "Age 15";    
+// alert(message);
+
+// var message;
+// message = "Certified Mobile Application Development";    
+// alert(message);
+
+// var message;
+// message = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+// alert(message);
+
+// var message;
+// message = "example@gmail.com";    
+// alert('My email address is ' + message );
+
+// var book;
+// book = " A Smarter \n Way to Learn JavaScript";    
+// alert('I am trying to learn from the Book ' + book );
+
+// document.write("Yah! I can write HTML content through Javascript");
+
+// var sign;
+// sign = "==========@#-&-#@==========";    
+// alert(sign);
